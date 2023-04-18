c840cf1d511167ce4f61761f8daf7bf3471ce4bc
Samuel Akabo <superakabo@gmail.com>
2023-04-18 20:59:00 +0000

    change previously selected assets from AssetEntity to asset Id string, fixed multi-select bug

--- lib/src/extensions/asset_path_entity_extension.dart
+++ lib/src/extensions/asset_path_entity_extension.dart
@@ -10,14 +10,4 @@ extension AssetPathEntityExtension on AssetPathEntity {
     final count = await assetCountAsync;
     return (count == 0) ? [] : await getAssetListRange(start: 0, end: count);
   }
-
-  Future<List<AssetEntity>> toAssetEntities(List<String> ids) async {
-    final assetEntities = <AssetEntity>[];
-    await Future.forEach(ids, (id) async {
-      final tmp = await AssetEntity.fromId(id);
-      final exists = (await tmp?.exists) ?? false;
-      if (exists) assetEntities.add(tmp!);
-    });
-    return assetEntities;
-  }
 }
